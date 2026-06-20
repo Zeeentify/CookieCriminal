32b8171f38d339ea3cfc924b25a0a2d887fbd1fe
Zeeentify <schweizeralejandro@gmail.com>
2026-06-20 14:49:13 +0200

    fixed more naming stuff

--- entities/player/camera_sway/camera_sway.gd
+++ entities/player/camera_sway/camera_sway.gd
@@ -1,11 +1,61 @@
-extends Camera3D
+extends Control
+
+@onready var button: Button = $Button
+@onready var button_2: Button = $Button2
+@export var camera_3d: Camera3D 
+@export var turn_speed: float = 0.1
+var is_hovering: bool = false
+var is_hovering2:bool = false
 
 
 # Called when the node enters the scene tree for the first time.
+
+
+
+
+func _lookleft(delta: float)-> void:
+	if camera_3d.rotation.y  > deg_to_rad(40):
+		return
+	print(camera_3d.rotation.y)
+	if is_hovering:
+		camera_3d.rotate_y(1.45 * delta)
+	
 func _ready() -> void:
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	
+	_lookleft(delta)
+		
+	if camera_3d.rotation.y  < deg_to_rad(-40):
+		return
+		
+	if is_hovering2:
+		camera_3d.rotate_y(-1.45 * delta)
+		
+
+
+func _on_button_mouse_entered() -> void:
+	
+	
+	is_hovering = true 
+
+func _on_button_mouse_exited() -> void:
+	is_hovering = false 
+
+
+ # Replace with function body.
+
+
+		
+func _on_button_2_mouse_entered() -> void:
+	
+	if camera_3d.rotation.y  <-40:
+		return
+	is_hovering2 = true
+
+
+func _on_button_2_mouse_exited() -> void:
+	is_hovering2 = false # Replace with function body.
--- globals/event_bus.gd
+++ globals/event_bus.gd
@@ -3,23 +3,23 @@ extends Node
 signal input_disabled
 signal input_enabled
 
-signal camera_input_disabled
-signal camera_input_enabled
+#signal camera_input_disabled
+#signal camera_input_enabled
 
 ##Dialogue
-signal dialogue_spoke(dialogue: String, theme: Theme) ##Dialogue Component to UI
-signal letter_revealed ##For SFX Purposes
-signal dialogue_finished
+#signal dialogue_spoke(dialogue: String, theme: Theme) ##Dialogue Component to UI
+#signal letter_revealed ##For SFX Purposes
+#signal dialogue_finished
 #Journal
-signal paged_view_opened
-signal paged_view_closed
-signal paged_view_changed_page(forward : bool)
-signal page_component_changed_page(new_image : Texture2D,new_page_number : int)
+#signal paged_view_opened
+#signal paged_view_closed
+#signal paged_view_changed_page(forward : bool)
+#signal page_component_changed_page(new_image : Texture2D,new_page_number : int)
 
 signal change_mouse_mode(new_mouse_mode : Input.MouseMode)
 
 
-signal interaction_target_changed(interaction_component : InteractionComponent)
+#signal interaction_target_changed(interaction_component : InteractionComponent)
 
 ##Director
 signal director_registered_actor(actor_name, actor)
